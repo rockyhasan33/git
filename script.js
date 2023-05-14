f288c28c905ab4265cd4cd116eae979f6566ab98
rockybul_hussain <rockyhasan403@gamil.com>
2023-05-14 16:15:22 +0530

    adding object

--- script.js
+++ script.js
@@ -2,3 +2,12 @@ let num1 = 10;
 let num2 = 5;
 
 console.log(num1 + num2);
+
+let obj = {
+    name: 'Rockybul Hussain',
+    age: 20,
+    gender: 'Male', 
+    nationality: 'Indian'
+}
+
+console.log(obj)
